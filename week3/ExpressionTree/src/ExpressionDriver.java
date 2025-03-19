4df4754b3a4892ad16ddcaad8d78ad1553f63d64
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2025-03-19 14:02:20 -0400

    Tried the 6 expressions.
    It doesn't run with division. since it's not coded for it.

---
+++ week3/ExpressionTree/src/ExpressionDriver.java
@@ -0,0 +1,38 @@
+class ExpressionDriver {
+
+   public static void main(String[] args) {
+   ExpressionParser myParser = new ExpressionParser();
+   ExpressionNode myNode;
+   String expression = null;
+
+   if (args.length != 1) {
+      System.out.println("Usage: java ExpressionDriver (+(3)(5))");
+      return;
+      } else {
+      expression = removeSpaces(args[0]);
+      myNode = myParser.parse(expression);
+   
+      System.out.println("Parse Tree: " + myNode);
+      System.out.println("Result: " + myNode.evaluate());
+      } // End if else
+
+   } // End main
+
+// -----------------------------------------------
+   private static String removeSpaces(String phrase) {
+   final char SPACE = ' ';
+   int i;
+
+   StringBuffer list = new StringBuffer(phrase);
+   i = 0;
+   while (i < list.length()) {
+      if (list.charAt(i) == SPACE)
+         list.deleteCharAt(i);
+      else
+         i++;
+      } // End while
+
+   return list.toString();
+   } // End method
+
+} // End class
